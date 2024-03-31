3b58edb0aabf85e276e8cdaff6ffe2814382c6e5
VIDHI AGARWAL <89720179+Vidhiagarwal25@users.noreply.github.com>
2024-03-31 23:06:37 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,36 @@
+let navar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('input[type="number"]').forEach(inputNumber =>{
+    inputNumber.oninput = () => {
+        if(inputNumber.ariaValueMax.length > inputNumber.maxlength)  inputNumber.value = inputNumber.value.slice(0, inputNumber.maxlength);
+    };
+});
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
